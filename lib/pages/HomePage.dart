003814454263ea0a6bfbfcf4e769ed829d401d97
ShristiC <shristi.chitlangia@utexas.edu>
2020-07-19 01:29:28 -0500

    created general input form

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -10,42 +10,98 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    ScreenUtil.init();
-    ScreenUtil.init(width: 750, height: 1334, allowFontScaling: true);
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              width: ScreenUtil().setWidth(75),
-              height: ScreenUtil().setHeight(75),
-              margin: ThemeStyle.marginALL,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage(Constants.LOGO_IMAGE_PATH),
-                  fit: BoxFit.fill,
-                ),
+  final _controller = TextEditingController();
+
+  ///designs the style for the App Bar on the homepage
+  Widget _appBar() {
+    return AppBar(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            width: ScreenUtil().setWidth(75),
+            height: ScreenUtil().setHeight(75),
+            margin: ThemeStyle.marginALL,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(Constants.LOGO_IMAGE_PATH),
+                fit: BoxFit.fill,
               ),
             ),
-            Text(
-              'Shardha Shinkara Seva',
-              style: ThemeStyle.appBarStyle,
-            ),
-            IconButton(
-              icon: Icon(Icons.account_circle),
-              color: ThemeStyle.whiteColor,
-              iconSize: 40,
-              onPressed: () {},
-            ),
-          ],
-        ),
-        backgroundColor: ThemeStyle.secondaryMainColor,
-        centerTitle: true,
+          ),
+          Text(
+            'Shardha Shinkara Seva',
+            style: ThemeStyle.appBarStyle,
+          ),
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            color: ThemeStyle.whiteColor,
+            iconSize: 40,
+            onPressed: () {},
+          ),
+        ],
       ),
+      backgroundColor: ThemeStyle.secondaryMainColor,
+      centerTitle: true,
+    );
+  }
+  
+  ///styling for the TextInputForm
+  Widget _textInputForm(String error, String label, IconData icon) {
+    return Container(
+      margin: ThemeStyle.marginALL,
+      // actual TextForm Field
+      child: TextFormField(
+        controller: _controller,
+        //validator creates a condition to be checked for when inputting response
+        validator: (value) {
+          return value.isEmpty ? error : null;
+        },
+        //creates the decoration for the input box
+        decoration: InputDecoration(
+          //outside border details
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: ThemeStyle.greyColor,
+              width: 2,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+          ),
+          //label text details
+          alignLabelWithHint: true,
+          labelText: label,
+          labelStyle: ThemeStyle.formLabelText,
+          //icon details
+          prefixIcon: Icon(
+            icon,
+            size: ThemeStyle.iconSize,
+            color: ThemeStyle.blackColor,
+          ),
+          //details when selecting that input box
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: ThemeStyle.primColor,
+              width: 2,
+            ),
+            borderRadius: ThemeStyle.borderRadiusStyle,
+          ),
+          //filling in the color of the inside input box
+          fillColor: ThemeStyle.lightGreyColor,
+          filled: true,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //helps control the screen sizing 
+    ScreenUtil.init();
+    ScreenUtil.init(width: 750, height: 1334, allowFontScaling: true);
+
+    return Scaffold(
+      appBar: _appBar(),
       resizeToAvoidBottomPadding: true,
       body: Center(
         child: ListView(
@@ -54,9 +110,16 @@ class _HomePageState extends State<HomePage> {
             Center(
               child: Text(
                 "Recipient Input Form",
-                style: ThemeStyle.heading.copyWith(color: Colors.black),
+                style: ThemeStyle.heading,
               ),
             ),
+            ///calls the textInputForm method to handle the user input
+            _textInputForm("Must enter a First Name", "First Name", Icons.person),
+            _textInputForm("Must enter a Last Name", "Last Name", Icons.person),
+            _textInputForm("Must enter a Adhar Pan Card Number", "Adhar Pan Card Number", Icons.account_balance_wallet),
+            _textInputForm("Must enter a City", "City", Icons.map),
+            _textInputForm("Must enter a State", "state", Icons.map),
+            _textInputForm("Must enter a Phone Number", "Phone Number", Icons.phone),
           ],
         ),
       ),
--- lib/utils/ThemeStyle.dart
+++ lib/utils/ThemeStyle.dart
@@ -11,28 +11,52 @@ class ThemeStyle {
   ///constrastColor = yellow tone
   static final contrastColor = Color(0xFFF9A825);
 
+  ///greyColor = dark grey tone
+  static final greyColor = Colors.grey[800];
+
+  ///lightGreyColor = light grey tone
+  static final lightGreyColor = Colors.grey[200];
+
   ///whiteColor = white tone
   static final whiteColor = Colors.white;
 
   ///blackColor = black tone
   static final blackColor = Colors.black;
 
-  static final marginALL = EdgeInsets.all(8);
+  ///marginAll = 8 pixel overall value
+  static final marginALL = EdgeInsets.all(10);
 
+  ///iconSize = 30 pixels
+  static final iconSize = 30.0;
+
+  ///borderRadiusStyle = Circular Radius of 30 pixels
+  static final borderRadiusStyle = BorderRadius.all(Radius.circular(30));
+
+  ///baseTextStyle = font is defined to be Roboto
   static final baseTextStyle = const TextStyle(
     fontFamily: 'Roboto',
   );
 
+  ///appBarStyle = to be used for the title in an AppBar
   static final appBarStyle = baseTextStyle.copyWith(
     color: whiteColor,
     fontSize: 20,
   );
 
+  ///heading --> Roboto font, size 24, .6 letter spacing, and bold 
   static final heading = baseTextStyle.copyWith(
     fontSize: 24,
     letterSpacing: .6,
     fontWeight: FontWeight.bold,
   );
+
+  ///formLabelText --> Roboto font, black color, size 16, .2 letter spacing
+  static final formLabelText = baseTextStyle.copyWith(
+    color: blackColor,
+    fontSize: 16,
+    letterSpacing: .2,
+  );
+
   
 
 }
