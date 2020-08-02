7e4087f5fceb0171d6a9eed101359cda0417b8e1
ShristiC <shristi.chitlangia@utexas.edu>
2020-08-02 18:02:34 -0500

    saving all the latest data

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
-import 'package:Shardha_Shinkara_Seva/pages/FinishedPage.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_database/firebase_database.dart';
+
+
+import 'package:Shardha_Shinkara_Seva/pages/FinishedPage.dart';
 import './pages/HomePage.dart';
 import './pages/Login.dart';
 import './pages/Settings.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // final FirebaseApp app = await FirebaseApp.configure(
+  //   name: 'Shardha Shinkara Seva App', 
+  //   options: const FirebaseOptions(
+  //     googleAppID: null
+  //   ),
+  // );
   runApp(MyApp());
 }
 
